690cfdf51146a3540b42d376551665f5c5945482
joshbrod <joshbrod@umich.edu>
2024-02-06 00:48:31 -0500

    Developed ability to respond to being @ed and implemented basic game choice response

--- bot.py
+++ bot.py
@@ -1,44 +1,80 @@
 # bot.py
 import os
-
 import discord
+from datetime import datetime, timedelta
 from discord.ext import commands
 from dotenv import load_dotenv
-import random
+from games import codenames
 
+# Private Key
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-intents = discord.Intents(messages=True, guilds=True)
+# Create the bot client
+intents = discord.Intents().all()
 intents.message_content = True
-# bot = discord.Client()
 bot = commands.Bot(command_prefix="!", intents=intents)
-   
+
+# Load info of games
+games = {"CodeNames": codenames}
+game_req = datetime.utcnow() - timedelta(seconds=10)
+user_req = ""
+
+
+async def start_game(channel, game_id):
+    """Runs the game chosen by the player."""
+    if game_id > len(games) or game_id <= 0:
+        out_msg = f"Error Not Valid Game:\nPlease give a number in the range of 1 to {len(games)}"
+        await channel.send(out_msg)
+        return
+    await games[sorted(games.keys())[game_id - 1]]()
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    """TODO: Check if the reaction is added to a specific message"""
+    print(f'Reaction {reaction.emoji} added by {user.name}')
+    print(reaction.message.id)
+
 @bot.event 
 async def on_ready(): 
+    """Controls functionality after running."""
     print("Logged in as a bot {0.user}".format(bot))
 
 @bot.event 
 async def on_message(message): 
-    username = str(message.author).split("#")[0] 
-    channel = str(message.channel.name) 
+    """Responds to messages."""
+    global game_req
+    global user_req
+    try:
+        username = message.author.nick
+    except:
+        username = str(message.author).split("#")[0]
+    try:
+        channel = str(message.channel.name) 
+    except:
+        channel = f"private_message_with_{username}"
     user_message = str(message.content) 
   
-    print(f'Message {user_message} by {username} on {channel}') 
-  
+    # Ignore messages from itself
     if message.author == bot.user: 
         return
-  
-    if channel == "random": 
-        if user_message.lower() == "hello" or user_message.lower() == "hi": 
-            await message.channel.send(f'Hello {username}') 
-            return
-        elif user_message.lower() == "bye": 
-            await message.channel.send(f'Bye {username}') 
-        elif user_message.lower() == "tell me a joke": 
-            jokes = ["""Can someone please shed more light on how my lamp got stolen?""", 
-                     """Why is she called llene? She stands on equal legs.""", 
-                     """What do you call a gazelle in a lions territory? Denzel."""] 
-            await message.channel.send(random.choice(jokes)) 
+    
+    # For debugging purposes
+    print(f'Message {user_message} by {username} on {channel}') 
+
+    # Handles responses to game chosen
+    if (datetime.utcnow() - game_req) <= timedelta(seconds=10) and user_req == username and user_message.isdigit():
+        await start_game(message.channel, int(user_message))
+
+    # Handles being summoned (@ed)
+    if "<@1204239173742108682>" in user_message.lower():
+        out_msg = f'Hello {username}, what game do you want to play today?\n'
+        out_msg += f'Respond in 10 seconds with one of the following number(s):\n'
+        for id, game in enumerate(sorted(games.keys())):
+            out_msg += "- " + game + ": " + str(id + 1) + "\n"
+        await message.channel.send(out_msg)
+        game_req = datetime.utcnow()
+        user_req = username
+        return
 
 bot.run(TOKEN)
---
+++ games.py
@@ -0,0 +1,2 @@
+async def codenames():
+    return
